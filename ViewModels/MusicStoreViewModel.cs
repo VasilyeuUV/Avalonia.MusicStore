508a97d1282852a1827f0afe15113129bfb37e0c
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-03-02 22:31:27 +0300

    vm: создание вьюмодель, привязка данных

--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,15 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
 
 namespace Avalonia.MusicStore.ViewModels
 {
     /// <summary>
     /// ViewModel диалогового окна "MusicStore"
     /// </summary>
-    public class MusicStoreViewModel
+    public class MusicStoreViewModel : ViewModelBase
     {
+        private string? _searchText;
+        private bool _isBusy;
+        private AlbumViewModel? _selectedAlbum;
+
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        public MusicStoreViewModel()
+        {
+            // Фейковые данные
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+        }
+
+        public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+
+        public string? SearchText
+        {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
+
+        public AlbumViewModel? SelectedAlbum
+        {
+            get => _selectedAlbum;
+            set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+        }
     }
 }
