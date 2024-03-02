67f2ed9f2af3287b4a36d56dc6a67fac5718c6d0
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-03-02 20:56:58 +0300

    view: Создание и добавление UserControl

---
+++ Views/UserControls/MusicStoreViewUserControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.MusicStore.Views.UserControls;
+
+public partial class MusicStoreViewUserControl : UserControl
+{
+    public MusicStoreViewUserControl()
+    {
+        InitializeComponent();
+    }
+}
